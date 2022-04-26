d0ec7b2afc865983c80ec3b2aedb1c5095729c48
Добрев Иван <trialerman@gmail.com>
2022-04-26 11:09:36 +0300

    Add project files.

---
+++ Warehouse.Core/TestViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Warehouse.Core.CustomAttributes;
+
+namespace Warehouse.Core
+{
+    public class TestViewModel
+    {
+        public DateTime FirstDate { get; set; }
+
+        [IsBefore(nameof(FirstDate), errorMessage: "Boom")]
+        public DateTime SecondDate { get; set; }
+    }
+}
