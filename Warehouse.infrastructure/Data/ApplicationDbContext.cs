a70b1f78239f61f66d7d85cff7db28c187c03a91
Добрев Иван <trialerman@gmail.com>
2022-05-30 22:21:51 +0300

    Data models added

--- Warehouse.infrastructure/Data/ApplicationDbContext.cs
+++ Warehouse.infrastructure/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Warehouse.infrastructure.Data;
 
 namespace Warehouse.Infrastructure.Data
 {
@@ -9,5 +10,16 @@ namespace Warehouse.Infrastructure.Data
             : base(options)
         {
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Item> Items { get; set; }
+
+        public DbSet<Rack> Racks { get; set; }
     }
 }
